f036eb3dc1c01570a1f4ca4c150df1cbf4e674e9
Mr Lima <felipebr4095@gmail.com>
2026-04-02 11:57:47 -0300

    I will finish it today

--- Lista 1 - Felipe/Lista 1 - Felipe/ex9.Designer.cs
+++ Lista 1 - Felipe/Lista 1 - Felipe/ex9.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lista_1___Felipe
 {
-    partial class ex8
+    partial class ex9
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ex8));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ex9));
             this.ex_Title = new System.Windows.Forms.Label();
             this.ex_Descriptor = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -36,10 +36,14 @@
             this.result_textBox = new System.Windows.Forms.TextBox();
             this.exitBtn = new System.Windows.Forms.Button();
             this.clearBtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.fireBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ex_Title
@@ -47,21 +51,21 @@
             this.ex_Title.AutoSize = true;
             this.ex_Title.BackColor = System.Drawing.Color.Transparent;
             this.ex_Title.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ex_Title.Location = new System.Drawing.Point(323, 28);
+            this.ex_Title.Location = new System.Drawing.Point(298, 27);
             this.ex_Title.Name = "ex_Title";
-            this.ex_Title.Size = new System.Drawing.Size(343, 40);
+            this.ex_Title.Size = new System.Drawing.Size(389, 40);
             this.ex_Title.TabIndex = 47;
-            this.ex_Title.Text = "Invertedor de Valores";
+            this.ex_Title.Text = "Calculadora de 4 valores";
             // 
             // ex_Descriptor
             // 
             this.ex_Descriptor.AutoSize = true;
             this.ex_Descriptor.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ex_Descriptor.Location = new System.Drawing.Point(341, 80);
+            this.ex_Descriptor.Location = new System.Drawing.Point(225, 78);
             this.ex_Descriptor.Name = "ex_Descriptor";
-            this.ex_Descriptor.Size = new System.Drawing.Size(307, 22);
+            this.ex_Descriptor.Size = new System.Drawing.Size(555, 22);
             this.ex_Descriptor.TabIndex = 48;
-            this.ex_Descriptor.Text = "Troca o que você escreve de posição";
+            this.ex_Descriptor.Text = "Realiza operações de adição e multiplicação com os quatro valores";
             // 
             // textBox1
             // 
@@ -69,17 +73,17 @@
             this.textBox1.Location = new System.Drawing.Point(28, 190);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(443, 75);
+            this.textBox1.Size = new System.Drawing.Size(197, 75);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(516, 190);
+            this.textBox2.Location = new System.Drawing.Point(259, 190);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(447, 75);
+            this.textBox2.Size = new System.Drawing.Size(212, 75);
             this.textBox2.TabIndex = 1;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -122,26 +126,6 @@
             this.clearBtn.UseVisualStyleBackColor = false;
             this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(197, 165);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 22);
-            this.label1.TabIndex = 53;
-            this.label1.Text = "Digite algo";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(660, 165);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(192, 22);
-            this.label2.TabIndex = 54;
-            this.label2.Text = "Digite algo novamente";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -164,19 +148,82 @@
             this.fireBtn.Size = new System.Drawing.Size(935, 66);
             this.fireBtn.TabIndex = 2;
             this.fireBtn.UseVisualStyleBackColor = false;
-            this.fireBtn.Click += new System.EventHandler(this.fireBtn_Click);
             // 
-            // ex8
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 22);
+            this.label1.TabIndex = 57;
+            this.label1.Text = "Digite um numero";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(516, 190);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 75);
+            this.textBox3.TabIndex = 56;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(768, 190);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 75);
+            this.textBox4.TabIndex = 58;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(287, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 22);
+            this.label2.TabIndex = 59;
+            this.label2.Text = "Digite um numero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(790, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 22);
+            this.label4.TabIndex = 60;
+            this.label4.Text = "Digite um numero";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(549, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 22);
+            this.label5.TabIndex = 61;
+            this.label5.Text = "Digite um numero";
+            // 
+            // ex9
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Moccasin;
             this.CancelButton = this.exitBtn;
             this.ClientSize = new System.Drawing.Size(986, 648);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.fireBtn);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.result_textBox);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -188,9 +235,9 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "ex8";
+            this.Name = "ex9";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "8. Invertedor de Valores";
+            this.Text = "9. Calculadora de 4 valores";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,9 +252,13 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox result_textBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button fireBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
---
+++ Lista 1 - Felipe/Lista 1 - Felipe/ex9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_1___Felipe
+{
+    public partial class ex9 : Form
+    {
+        public ex9()
+        {
+            InitializeComponent();
+        }
+
+        private void fireBtn_example(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text))
+            {
+                result_textBox.Text = "Preencha todos os campos deste formulario para realizar a operação!";
+            }
+        }
+        private void clearBtn_Click(object sender, EventArgs e) { textBox1.Text = ""; textBox2.Text = ""; result_textBox.Text = ""; }
+        private void exitBtn_Click(object sender, EventArgs e) { this.Close(); }
+    }
+}
+/*
+9. Faça um algoritmo que leia quatro números e apresente os resultados de adição e multiplicação dos valores 
+entre si, baseando-se na utilização da propriedade distributiva, ou seja, se forem lidas as variáveis A, B, C e D, 
+devem ser somadas e multiplicadas A com B, A com C e A com D; B com C, B com D e por último C com D.
+ */
