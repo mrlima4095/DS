e725380f1dba0dd09b9a243f10e9306f4d8c88c3
Mr Lima <felipebr4095@gmail.com>
2026-04-10 09:51:07 -0300

    Updated

--- Lista 1 - Felipe/Lista 1 - Felipe/Menu.cs
+++ Lista 1 - Felipe/Lista 1 - Felipe/Menu.cs
@@ -32,126 +32,165 @@ namespace Lista_1___Felipe
         private void ex1_choiceBtn_Click(object sender, EventArgs e)
         {
             ex1 Exercicio1 = new ex1();
+            Exercicio1.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio1.Show();
         }
 
         private void ex2_choiceBtn_Click(object sender, EventArgs e)
         {
             ex2 Exercicio2 = new ex2();
+            Exercicio2.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio2.Show();
         }
 
         private void ex3_choiceBtn_Click(object sender, EventArgs e)
         {
             ex3 Exercicio3 = new ex3();
+            Exercicio3.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio3.Show();
         }
 
         private void ex4_choiceBtn_Click(object sender, EventArgs e)
         {
             ex4 Exercicio4 = new ex4();
+            Exercicio4.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio4.Show();
         }
 
         private void ex5_choiceBtn_Click(object sender, EventArgs e)
         {
             ex5 Exercicio5 = new ex5();
+            Exercicio5.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio5.Show();
         }
 
         private void ex6_choiceBtn_Click(object sender, EventArgs e)
         {
             ex6 Exercicio6 = new ex6();
+            Exercicio6.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio6.Show();
         }
 
         private void ex7_choiceBtn_Click(object sender, EventArgs e)
         {
             ex7 Exercicio7 = new ex7();
+            Exercicio7.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio7.Show();
         }
 
         private void ex8_choiceBtn_Click(object sender, EventArgs e)
         {
             ex8 Exercicio8 = new ex8();
+            Exercicio8.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio8.Show();
         }
 
+        private void ex9_choiceBtn_Click(object sender, EventArgs e)
+        {
+            ex9 Exercicio9 = new ex9();
+            Exercicio9.FormClosed += (s, args) => this.Show();
+            Exercicio9.Show();
+        }
         private void ex10_choiceBtn_Click(object sender, EventArgs e)
         {
             ex10 Exercicio10 = new ex10();
+            Exercicio10.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio10.Show();
         }
 
         private void ex11_choiceBtn_Click(object sender, EventArgs e)
         {
             ex11 Exercicio11 = new ex11();
+            Exercicio11.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio11.Show();
         }
 
         private void ex12_choiceBtn_Click(object sender, EventArgs e)
         {
             ex12 Exercicio12 = new ex12();
+            Exercicio12.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio12.Show();
         }
 
         private void ex13_choiceBtn_Click(object sender, EventArgs e)
         {
             ex13 Exercicio13 = new ex13();
+            Exercicio13.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio13.Show();
         }
 
         private void ex14_choiceBtn_Click(object sender, EventArgs e)
         {
             ex14 Exercicio14 = new ex14();
+            Exercicio14.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio14.Show();
         }
 
         private void ex15_choiceBtn_Click(object sender, EventArgs e)
         {
             ex15 Exercicio15 = new ex15();
+            Exercicio15.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio15.Show();
         }
 
         private void ex16_choiceBtn_Click(object sender, EventArgs e)
         {
             ex16 Exercicio16 = new ex16();
+            Exercicio16.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio16.Show();
         }
 
         private void ex17_choiceBtn_Click(object sender, EventArgs e)
         {
             ex17 Exercicio17 = new ex17();
+            Exercicio17.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio17.Show();
         }
 
         private void ex18_choiceBtn_Click(object sender, EventArgs e)
         {
             ex18 Exercicio18 = new ex18();
+            Exercicio18.FormClosed += (s, args) => this.Show();
             Exercicio18.Show();
         }
         private void ex19_choiceBtn_Click(object sender, EventArgs e)
         {
             ex19 Exercicio19 = new ex19();
+            Exercicio19.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio19.Show();
         }
 
         private void ex20_choiceBtn_Click(object sender, EventArgs e)
         {
             ex20 Exercicio20 = new ex20();
+            Exercicio20.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio20.Show();
         }
 
         private void ex21_choiceBtn_Click(object sender, EventArgs e)
         {
             ex21 Exercicio21 = new ex21();
+            Exercicio21.FormClosed += (s, args) => this.Show();
+            this.Hide();
             Exercicio21.Show();
-        }
-
-        private void ex9_choiceBtn_Click(object sender, EventArgs e)
-        {
-            ex9 Exercicio9 = new ex9();
-            Exercicio9.Show();
         }
     }
 }
