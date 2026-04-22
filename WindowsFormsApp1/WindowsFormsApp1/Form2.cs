071c61935f28099eea4f1bf29209d778529b99eb
Mr Lima <felipebr4095@gmail.com>
2026-04-22 13:33:25 -0300

    WindowsFormsApp - Operadores Logicos

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = Int32.Parse(textBox1.Text);
+            double altura = Convert.ToDouble(textBox2.Text);
+
+            if ((idade >= 15) && (idade <= 25) && (altura >= 1.60))
+            {
+                textBox3.Text = "Sim" + Environment.NewLine + ""; 
+            }
+            else
+            {
+                textBox3.Text = "Não pode entrar na equipe de natação";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
