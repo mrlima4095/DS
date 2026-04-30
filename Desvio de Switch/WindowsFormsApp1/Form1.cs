2f5a90cfe03338202eb3eb2acee4949f9f429aeb
Mr Lima <felipebr4095@gmail.com>
2026-04-30 11:52:01 -0300

    3

---
+++ Desvio de Switch/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 and = new Form2();
+            and.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 or = new Form3();
+            or.Show();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 and_or = new Form4();
+            and_or.Show();
+        }
+    }
+}
